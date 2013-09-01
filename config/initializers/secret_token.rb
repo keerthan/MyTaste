6ba7d2da1d82d9818d0599911c58d39586d8f741
Keerthan Muthurasa <muthurasa.keerthan@gmail.com>
2013-09-01 20:01:15 +0200

    Added rails project structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTaste::Application.config.secret_token = 'a2a1014cceb17c83b6cc8b96ec0ed02e57524f94a8740c70b52cf9d9af336be83807e64daae188219a931f7d5d16e2b49d6f81db90058532567cfa8064c7cffe'
